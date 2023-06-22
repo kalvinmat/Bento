850ef2ae9897116f9ee15b7ad06ae80b148342e9
kalvinmat <kalvinmateo16@gmail.com>
2023-06-21 17:23:09 -0700

    changed one dark to nord

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: '2bdaf4fd1920faa9947d1684cf937cce', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'F', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
